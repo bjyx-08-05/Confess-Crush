cd2736dd32b1c34bb81301601d7e23eef0b5e13b
bjyx-08-05 <94051676+bjyx-08-05@users.noreply.github.com>
2021-11-10 21:39:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Gửi cho VPL",
-    introTitle: '......',
+    titleWeb: "Gửi cho Tạ Phương Khải",
+    introTitle: 'Tạ Phương Khải',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Yêu em ',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100034521067957
+    reply: 'Bởi vì em đáng yêu ',
+    mess: 'Em biết mà . Yêu anh',
+    messDesc: 'Hôm nào rảnh mình gặp nhau nha.',
+    btnAccept: 'Dạ <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100007999451481
 }
